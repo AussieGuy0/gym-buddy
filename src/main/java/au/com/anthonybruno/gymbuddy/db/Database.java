4062a3fdce788e0a0a777b6c968c1378cf945ef2
AussieGuy0 <anthony.bruno196@gmail.com>
2018-03-30 15:28:17 +1030

    Add postgres dependency, add rename Connection class

--- src/main/java/au/com/anthonybruno/gymbuddy/db/Database.java
+++ src/main/java/au/com/anthonybruno/gymbuddy/db/Database.java
@@ -1,6 +1,25 @@
 package au.com.anthonybruno.gymbuddy.db;
 
-public class Connection {
+import javax.naming.InitialContext;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-    
+public class Database {
+
+    private final String username;
+    private final String password;
+    private final String url;
+
+    public Database(String username, String password, String url) {
+        this.username = username;
+        this.password = password;
+        this.url = url;
+    }
+
+    private Connection getConnection() throws SQLException {
+        Connection connection;
+        return DriverManager.getConnection(url, username, password);
+    }
+
 }
