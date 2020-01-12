1a43c75273e686a62dd9f5ea2aef7a79d36b3529
AussieGuy0 <anthony.bruno196@gmail.com>
2020-01-12 17:24:42 +1030

    Add exercises seed data

---
+++ backend/src/main/resources/db/migration/V2__seed-exercises.sql
@@ -0,0 +1,29 @@
+-- Generated from exercises.csv
+
+COPY public.exercises (name, description, main_muscle) FROM stdin;
+Barbell Squat	\N	Legs
+Dumbbell Lunges	\N	Legs
+Leg Curls	\N	Legs
+Leg Press	\N	Legs
+Calf Raises	\N	Legs
+Leg Extension	\N	Legs
+Standing Calf Raise	\N	Legs
+Sitting Calf Raise	\N	Legs
+Barbell Bench Press	\N	Chest
+Dumbbell Bench Press	\N	Chest
+Chest Fly	\N	Chest
+Pull-up	\N	Back
+Pull-down	\N	Back
+Bent-over Row	\N	Back
+Deadlift	\N	Back
+Barbell Shoulder Press	\N	Shoulders
+Seated Dumbbell Shoulder Press	\N	Shoulders
+Shoulder Fly	\N	Shoulders
+Shoulder Shrug	\N	Shoulders
+Upright Row	\N	Shoulders
+Bicep Curl	\N	Biceps
+Hammer Curl	\N	Biceps
+Leg Raise	\N	Abs
+\.
+
+
