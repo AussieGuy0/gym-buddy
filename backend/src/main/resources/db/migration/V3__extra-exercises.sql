397a0ec6c8defa62b5ba441a7494e39f9f66bfb2
AussieGuy0 <anthony.bruno196@gmail.com>
2020-02-24 21:46:38 +1030

    Add additional exercises

---
+++ backend/src/main/resources/db/migration/V3__extra-exercises.sql
@@ -0,0 +1,8 @@
+INSERT INTO exercises (name, description, main_muscle)
+VALUES ('Seated Cable Row', null, 'Back'),
+       ('Chest Press Machine', null, 'Chest'),
+       ('Romanian Deadlift', null, 'Legs'),
+       ('Triceps Pushdown', null, 'Triceps'),
+       ('Overhead Triceps Extension', null, 'Triceps'),
+       ('Bench dips', null, 'Triceps')
+
