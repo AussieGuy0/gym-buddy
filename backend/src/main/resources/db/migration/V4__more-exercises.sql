018b2201677ad9f8f14b1fb0082723f30f35f9e0
AussieGuy0 <anthony.bruno196@gmail.com>
2020-08-24 20:51:46 +0930

    Add more exercises

---
+++ backend/src/main/resources/db/migration/V4__more-exercises.sql
@@ -0,0 +1,5 @@
+INSERT INTO exercises (name, description, main_muscle)
+VALUES ('Face Pull', null, 'Back'),
+       ('Incline Dumbbell Bench Press', null, 'Chest'),
+       ('Machine Fly', null, 'Chest')
+
